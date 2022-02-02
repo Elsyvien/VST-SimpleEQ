61a50346b5fc3c493d4f492f8ca9f0f433ba2fbf
Max <max@staneker.com>
2022-02-02 16:39:04 +0100

    Implemented button classes

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -310,6 +310,8 @@ private:
         lowCutSlopeSliderAttachment,
         highCutSlopeSliderAttachment;
 
+    juce::ToggleButton lowcutBypassButton, highcutBypassButton, peakBypassButton, analyzerEnabledButton;
+
     std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
